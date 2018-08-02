9e486bedb0e32a8347362283c9892a0dc051a89d
Evan Pratten <ewpratten@gmail.com>
2018-08-02 11:14:56 -0400

    build 1

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from setuptools import setup
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='devRantSimple',
     version='1.0',
     description='A simple devRant api wrapper for the lazy people',
-    long_description=open("README.rst").read(),
     url='https://github.com/Ewpratten/devRantSimple',
     author='Evan Pratten',
     author_email='ewpratten@gmail.com',
     license='MIT',
-    packages=['devRantSimple'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
     install_requires=['requests', 'enum34'],
-    zip_safe=False)
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ),
+)
--- tests/devRantSimple-livetest.py
+++ tests/devRantSimple-livetest.py
@@ -2,10 +2,11 @@ import sys
 sys.path.append('../devRantSimple')
 import devRantSimple as dRS
 
-# print(dRS.getUserRant("ewpratten", 10000))
+# print(dRS.getUserRant("ewpratten", 1))
 
 prevdata = ""
 i = 0
 while prevdata != dRS.InvalidResponse:
-	print(dRS.getUserRant("ewpratten", i)["text"])
+	prevdata = dRS.getUserRant("ewpratten", i)["text"]
+	print(prevdata)
 	i+=1
