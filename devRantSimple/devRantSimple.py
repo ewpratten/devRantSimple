847bb6c9020bec7bfa289db40bbab03c10b7da64
Evan Pratten <ewpratten@gmail.com>
2018-08-03 12:37:10 -0300

    updates

--- devRantSimple/devRantSimple.py
+++ devRantSimple/devRantSimple.py
@@ -99,4 +99,8 @@ def login(username, password):
 def postRant(text, tags, uid, token, key):
 	rsp = requests.post("https://devrant.com/api/devrant/rants", data={"app":3, "user_id":uid, "token_id":token, "token_key":key, "rant":text, "tags":tags})
 	return rsp
+
+def comment(rantid, text, uid, token, key):
+	rsp = requests.post("https://devrant.com/api/devrant/rants" + str(rantid) + "/comments", data={"app":3, "user_id":uid, "token_id":token, "token_key":key, "comment":text})
+	return rsp
 # debug
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='devRantSimple',
-    version='1.4',
+    version='1.7',
     description='A simple devRant api wrapper for the lazy people',
     url='https://github.com/Ewpratten/devRantSimple',
     author='Evan Pratten',
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=['requests', 'enum34'],
+    install_requires=['requests'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
